223202402fbacd55bd0531f4fb7a7b052425f4c2
Arunov1 <110475549+Arunov1@users.noreply.github.com>
2023-03-05 15:35:59 +0530

    Update MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -1,1 +1,25 @@
+public class MatrixAddition {
+   public static void main(String[] args) {
+      int rows = 3, columns = 3;
+      int[][] firstMatrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+      int[][] secondMatrix = { {9, 8, 7}, {6, 5, 4}, {3, 2, 1} };
+
+      // Adding matrices
+      int[][] sum = new int[rows][columns];
+      for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < columns; j++) {
+            sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+         }
+      }
+
+      // Displaying the result
+      System.out.println("Sum of matrices is: ");
+      for (int[] row : sum) {
+         for (int column : row) {
+            System.out.print(column + "    ");
+         }
+         System.out.println();
+      }
+   }
+}
 
